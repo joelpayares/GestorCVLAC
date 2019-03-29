61cb245e5c7b211de88dd38e4cb3953ea4d64757
joelpayares <joel.payares@cecar.edu.co>
2019-03-29 11:53:11 -0500

    Update EstadisticasActivity.java
    Actualización Estadisticas

--- app/src/main/java/edu/cecar/controladores/EstadisticasActivity.java
+++ app/src/main/java/edu/cecar/controladores/EstadisticasActivity.java
@@ -1,21 +1,21 @@
 package edu.cecar.controladores;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.EditText;
 
 public class EstadisticasActivity extends AppCompatActivity {
-
-    EditText txtconhom;
-    EditText txtconmuj;
-    EditText txtconcat;
-    EditText txtconsincat;
-    EditText txtconlininv;
-    EditText txtconsininv;
+    TextInputEditText txtconhom;
+    TextInputEditText txtconmuj;
+    TextInputEditText txtconcat;
+    TextInputEditText txtconsincat;
+    TextInputEditText txtconlininv;
+    TextInputEditText txtconsininv;
 
     Investigador objconinv;
-    int conhom=0,conmuj=0, concat=0,consincat=0, conlininv=0,consininv=0;
+    int conhom = 0, conmuj = 0, concat = 0, consincat = 0, conlininv = 0, consininv = 0;
+    private boolean conest = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,73 +31,58 @@ public class EstadisticasActivity extends AppCompatActivity {
 
         Investigadores objdatinv = new Investigadores(0);
 
-        for (int i=0;i<5;i++)
-        {
-            extraerInvestigador(objdatinv.matdatinv[i].urlinv);
+        for (int i = 0; i < 5; i++) {
+            extraerInvestigador(objdatinv.matdatinv[i].urlinv, i);
         }
 
-        Log.i("Hombres", "" + conhom);
+        runOnUiThread (new Thread(new Runnable() {
+            public void run() {
+                boolean loccon = false;
 
-//        txtconhom.setText(conhom);
-//        txtconmuj.setText(conmuj);
-//        txtconcat.setText(concat);
-//        txtconsincat.setText(consincat);
-//        txtconlininv.setText(conlininv);
-//        txtconsininv.setText(consininv);
+                while(!loccon){
+                    if (conest == true){
+                        loccon = true;
+
+                        txtconhom.setText("" + String.valueOf(conhom));
+                        txtconmuj.setText("" + String.valueOf(conmuj));
+                        txtconcat.setText("" + String.valueOf(concat));
+                        txtconsincat.setText("" + String.valueOf(consincat));
+                        txtconlininv.setText("" + String.valueOf(conlininv));
+                        txtconsininv.setText("" + String.valueOf(consininv));
+                    }
+                }
+            }
+        }));
     }
 
-    private void extraerInvestigador(final String parurlinv) {
+    private synchronized void extraerInvestigador(final String parurlinv, final int parconinv) {
         new Thread(new Runnable() {
             @Override
             public void run() {
-
-                Log.i("1", "Antes de extraer datos de investigador");
-
                 objconinv = ExtraerDatoCVLAC.getDatosH3(parurlinv);
 
-                Log.i("2", "Despues de extraer datos de investigador");
+                if (objconinv.getSexo().trim().charAt(0) == 'M') {
+                    conhom = conhom + 1;
+                } else if (objconinv.getSexo().trim().charAt(0) == 'F') {
+                    conmuj = conmuj + 1;
+                }
 
-                extraerEstadisticas(objconinv);
+                if (objconinv.isCategorizado() == true) {
+                    concat = concat + 1;
+                } else {
+                    consincat = consincat + 1;
+                }
+
+                if (objconinv.getLineas().length > 0) {
+                    conlininv = conlininv + 1;
+                } else {
+                    consininv = consininv + 1;
+                }
+
+                if (parconinv == 4){
+                    conest = true;
+                }
             }
         }).start();
-    }
-
-
-    public void extraerEstadisticas(Investigador objconinv) {
-
-//        if (objconinv == null)
-//            Log.i("3", "No se ha inicializado el investigador " + i);
-//        else
-//            Log.i("4", "Se ha inicializado el investigador " + i);
-
-        if (objconinv.getSexo().trim().charAt(0)=='M')
-        {
-            conhom=conhom+1;
-        }
-        else
-        {
-            if (objconinv.getSexo().trim().charAt(0)=='F')
-            {
-                conmuj=conmuj+1;
-            }
-        }
-
-        if (objconinv.isCategorizado()==true)
-        {
-            concat=concat+1;
-        }
-        else
-        {
-            consincat=consincat+1;
-        }
-
-        if (objconinv.getLineas().length>0)
-        {
-            conlininv=conlininv+1;
-        }
-        else
-        {
-            consininv=consininv+1;
-        }
     }
 }

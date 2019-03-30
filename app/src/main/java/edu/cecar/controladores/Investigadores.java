fe4289e5cfaa2d98e4954358076213f8feb54a64
Joel Payares <joel.payares@cecar.edu.co>
2019-03-29 20:09:14 -0500

    consultando estadisticas de los investigadores

--- app/src/main/java/edu/cecar/controladores/Investigadores.java
+++ app/src/main/java/edu/cecar/controladores/Investigadores.java
@@ -2,6 +2,7 @@ package edu.cecar.controladores;
 
 public class Investigadores {
     DatosInvestigadores matdatinv[];
+    private static final String CONST_MAS = "Masculino";
 
     public Investigadores(int modo) //modo=0 producción, modo=1 testeo
     {
@@ -24,10 +25,10 @@ public class Investigadores {
 
     public void cargar_datos_investigadores_testeo() {
 
-        matdatinv[0] = new DatosInvestigadores("http://scienti.colciencias.gov.co:8081/cvlac/visualizador/generarCurriculoCv.do?cod_rh=0001376707", "Guillermo Carlos Hernández Hernández", "Masculino", false, 0);
+        matdatinv[0] = new DatosInvestigadores("http://scienti.colciencias.gov.co:8081/cvlac/visualizador/generarCurriculoCv.do?cod_rh=0001376707", "Guillermo Carlos Hernández Hernández", CONST_MAS, false, 0);
         matdatinv[1] = new DatosInvestigadores("http://scienti.colciencias.gov.co:8081/cvlac/visualizador/generarCurriculoCv.do?cod_rh=0000402478", "Luty Del Carmen Gomezcaceres Peréz", "Femenino", true, 3);
-        matdatinv[2] = new DatosInvestigadores("http://scienti.colciencias.gov.co:8081/cvlac/visualizador/generarCurriculoCv.do?cod_rh=0000733180", "Jhon Jaime Mendez Alandete", "Masculino", false, 1);
-        matdatinv[3] = new DatosInvestigadores("http://scienti.colciencias.gov.co:8081/cvlac/visualizador/generarCurriculoCv.do?cod_rh=0000787132", "Namuel Francisco Solórzano Peralta", "Masculino", false, 0);
+        matdatinv[2] = new DatosInvestigadores("http://scienti.colciencias.gov.co:8081/cvlac/visualizador/generarCurriculoCv.do?cod_rh=0000733180", "Jhon Jaime Mendez Alandete", CONST_MAS, false, 1);
+        matdatinv[3] = new DatosInvestigadores("http://scienti.colciencias.gov.co:8081/cvlac/visualizador/generarCurriculoCv.do?cod_rh=0000787132", "Namuel Francisco Solórzano Peralta", CONST_MAS, false, 0);
         matdatinv[4] = new DatosInvestigadores("http://scienti.colciencias.gov.co:8081/cvlac/visualizador/generarCurriculoCv.do?cod_rh=0001414537", "Adriana Patricia Arboleda Lopez", "Femenino", true, 5);
     }
 }
